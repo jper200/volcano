c9cadabc7a5d3509a6cff6de61e37ec90ad59370
jper200 <112582736+jper200@users.noreply.github.com>
2022-09-13 19:18:22 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,78 @@
+// magma rising
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(200)
+}
+// lava erupting
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(200)
+}
